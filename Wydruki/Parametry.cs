97162c42fa9ff94937e8a08f2669cf02cfda8f09
Bartosz Zalewski <143065049+bzalewskifx2@users.noreply.github.com>
2023-10-10 12:06:25 +0200

    Update Parametry.cs

--- Wydruki/Parametry.cs
+++ Wydruki/Parametry.cs
@@ -4,36 +4,36 @@ using Soneta.Tools;
 
 namespace Soneta.Handel.Reports
 {
-		public class soid_umowaSnippet : ReportSnippet
+    public class soid_umowaSnippet : ReportSnippet
+    {
+	public class Params : ContextBase//SerializableContextBase - umożliwia zapamiętanie domyślnych wartości parametrów
+	{
+	    public Params(Context context) : base(context) { }
+	
+	    private int year = DateTime.Now.Year;
+	
+	    [Required]
+	    [Caption("Rok"), DefaultWidth(4)]
+	    public int Year
+	    {
+		get => year;
+	
+		set
 		{
-				public class Params : ContextBase//SerializableContextBase - umożliwia zapamiętanie domyślnych wartości parametrów
-				{
-				    public Params(Context context) : base(context) { }
-				
-				    private int year = DateTime.Now.Year;
-				
-				    [Required]
-				    [Caption("Rok"), DefaultWidth(4)]
-				    public int Year
-				    {
-				        get => year;
-				
-				        set
-				        {
-				            year = value;
-				            OnChanged(EventArgs.Empty);
-				        }
-				    }
-				}
-			  
-				[Context]
-				public Params BaseParams { get; set; }
+		    year = value;
+		    OnChanged(EventArgs.Empty);
+		}
+	    }
+	}
+  
+	[Context]
+	public Params BaseParams { get; set; }
 
         [DxBind]
-				private readonly DevExpress.XtraReports.UI.DetailBand DokumentTreść;
+	private readonly DevExpress.XtraReports.UI.DetailBand DokumentTreść;
 
         [DxBind]
-				private void DokumentTreść_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+	private void DokumentTreść_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
         {
             // akcja
         }
