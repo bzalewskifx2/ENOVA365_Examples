5340496dbd9ae653cdd96dbf01b5b9c8f72bb73f
Bartosz Zalewski <143065049+bzalewskifx2@users.noreply.github.com>
2023-10-17 14:13:58 +0200

    Update PracHistoria.cs

--- Kadry/PracHistoria.cs
+++ Kadry/PracHistoria.cs
@@ -1,4 +1,3 @@
 // *** aktualna PracHistoria *** //
-Pracownik prac = Grid1.GetCurrentRow() as Pracownik;
-Date aktualny = aktualny = ((ActualDate) dc.Context[typeof(ActualDate)]).Actual;
-PracHistoria ph = prac[aktualny];
+Pracownik prac = null;;
+PracHistoria ph = prac[Date.Now];
