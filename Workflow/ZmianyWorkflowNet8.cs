36c07b51d98a6644296d55ee1f2a44390756f72c
Bartosz Zalewski <143065049+bzalewskifx2@users.noreply.github.com>
2024-08-08 14:54:56 +0200

    Update ZmianyWorkflowNet8.cs

--- Workflow/ZmianyWorkflowNet8.cs
+++ Workflow/ZmianyWorkflowNet8.cs
@@ -10,3 +10,13 @@ public static WizardStepListSelectionInfo Create(object[] selected) została zas
 public static WizardStepListSelectionInfo Create(object[] selected, object focused) została zastąpiona przez Create(Context.Empty, selected, focused, "").
 public object[] Selected został zastąpiony przez metodę GetSelected(Guid stepGuid, string gridId).
 public object Focused został zastąpiony przez metodę GetFocused(Guid stepGuid, string gridId).
+
+
+// Przykład:
+public object GetListForListStep() 
+{
+    WizardStepListSelectionInfo selected = Context[typeof(WizardStepListSelectionInfo)] as WizardStepListSelectionInfo;
+    var wiz = Context[typeof(WizardDefinitionProxy)] as WizardDefinitionProxy;
+    WizardStepDefinition[] steps = wiz.Definition.Steps.ToArray();
+    return selected.GetSelected(steps[0].Guid, ""); 
+}
